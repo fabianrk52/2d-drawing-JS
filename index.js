7d0d90c434b3c0514242d4abe8b3f09a3c203a90
fabianrk52 <fabianrk@gmail.com>
2019-04-17 01:41:10 -0500

    adding bezier

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ document.addEventListener("click", function(){
     if(Coords.length==4){
         MyCircle();
     }
-
+    if(Coords.length==8){
+        myline();
+    }
   });
 
   function init(){
@@ -36,18 +38,32 @@ document.addEventListener("click", function(){
   function MyLine(){
     var canvas = document.getElementById("mainCanvas");
     var ctx = canvas.getContext("2d");
+    ctx.beginPath();
     ctx.moveTo(Coords[0].x, Coords[0].y);
     ctx.lineTo(Coords[1].x, Coords[1].y);
+    ctx.closePath();
     ctx.stroke();
   }
 
   function MyCircle(){
     var canvas = document.getElementById("mainCanvas");
     var ctx = canvas.getContext("2d");
+    ctx.beginPath();
     var a = Coords[2].x - Coords[3].x;
     var b = Coords[2].y - Coords[3].y;
     var r = Math.sqrt( a*a + b*b );
     ctx.arc(Coords[2].x,Coords[2].y, r, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.stroke();
+  }
+
+  function myline(){
+    var canvas = document.getElementById("mainCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(Coords[4].x, Coords[4].y);
+    ctx.bezierCurveTo(Coords[5].x, Coords[5].y, Coords[6].x, Coords[6].y, Coords[7].x, Coords[7].y);
+    ctx.closePath();
     ctx.stroke();
   }
 
