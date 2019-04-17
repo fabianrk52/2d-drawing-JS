84416dcdacd385347da5e3a059df86bb7a72d91f
fabianrk52 <fabianrk@gmail.com>
2019-04-16 20:40:17 -0500

    adding MyCircle()

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ class Coord {
         this.y = y;
     }
   }
-var c = document.getElementById("mainCanvas");
 
 document.addEventListener("click", function(){
     init();
@@ -13,9 +12,14 @@ document.addEventListener("click", function(){
     if(Coords.length==2){
         MyLine();
     }
+    if(Coords.length==4){
+        MyCircle();
+    }
+
   });
+
   function init(){
-    canvas = document.getElementById("mainCanvas");
+    var canvas = document.getElementById("mainCanvas");
     canvas.width = document.documentElement.clientWidth;
     canvas.height = document.documentElement.clientHeight;
 }
@@ -30,10 +34,20 @@ document.addEventListener("click", function(){
   }
 
   function MyLine(){
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
+    var canvas = document.getElementById("mainCanvas");
+    var ctx = canvas.getContext("2d");
     ctx.moveTo(Coords[0].x, Coords[0].y);
     ctx.lineTo(Coords[1].x, Coords[1].y);
+    ctx.stroke();
+  }
+
+  function MyCircle(){
+    var canvas = document.getElementById("mainCanvas");
+    var ctx = canvas.getContext("2d");
+    var a = Coords[2].x - Coords[3].x;
+    var b = Coords[2].y - Coords[3].y;
+    var r = Math.sqrt( a*a + b*b );
+    ctx.arc(Coords[2].x,Coords[2].y, r, 0, 2 * Math.PI);
     ctx.stroke();
   }
 
